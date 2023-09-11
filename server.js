21b0fc46be5625c12612f90368140edc4c3ff12b
sam-c14 <samuelaffah14@gmail.com>
2023-09-11 20:30:27 +0100

    Changes made to server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+require("dotenv").config();
 
 app.use(cors);
 app.use(express.json());
