9c25bef9a45208e53382b7b6c7ef37999da25b86
sam-c14 <samuelaffah14@gmail.com>
2023-10-05 14:07:57 +0100

    Changes made for webhook test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const bodyParser = require('body-parser')
 const port = process.env.PORT || 5000;
 
 require("dotenv").config();
@@ -40,7 +41,6 @@ app.get("/api", (req, res) => {
     currentUtcTimeDate <= maxValidTime
   ) {
     date = new Date().toISOString();
-    console.log(date);
     utcTime = date.split(".")[0] + date[date.length - 1];
   } else {
     utcTime = "Current UTC time is outside the valid range.";
@@ -58,6 +58,37 @@ app.get("/api", (req, res) => {
   });
 });
 
+app.post("/api/v1/webhook-test", (req, res) => {
+  // Handle the webhook payload here
+  const payload = req.body;
+  console.log("Webhook payload:", payload);
+
+  // Add your custom logic to process the payload
+
+  // Respond to the webhook request
+  res.status(200).send("Webhook received successfully");
+});
+
 app.listen(port, () => {
   console.log("App listening on port " + port);
 });
+
+// function getFactorial(n) {
+//   if (n !== 1) return n * getFactorial(n - 1);
+//   return 1;
+// }
+// const test = "abcabcabcs";
+// const testVar = new RegExp("abc", "g");
+// console.log(test.replace(testVar, ""));
+// const tetso = "abcabcabc";
+// console.log(test === tetso);
+// console.log(getFactorial(6));
+// const testVar = "dynamicValue"; // Replace with your variable value
+// const pattern = new RegExp(testVar, "g");
+
+// const inputString =
+//   "This is a sample string with some dynamicValue words. dynamicValue is repeated several times.";
+
+// const replacedString = inputString.replace(pattern, "replacement");
+
+// console.log(replacedString);
